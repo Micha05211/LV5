3b9c9fc0fb4403c4e5068b947ba7605d94b4f6d8
Hanzić Michael <0135279869@FPZ.HR>
2024-11-14 11:25:06 +0100

    Dodavanje provjere dva broja unutar raspona [10, 100]

--- zadatak2.py
+++ zadatak2.py
@@ -1,13 +1,17 @@
 def provjera_broja(broj):
-    if 10 <= broj <= 100:
+    if 10 <= 100:
         return f"Broj {broj} je unutar raspona."
     
     else:
         return f"Broj {broj} je izvan raspona."
     
+
 if __name__ == "__main__":
     try:
-        uneseni_broj = int(input("Unesite broj: "))
-        print(provjera_broja(uneseni_broj))
+        prvi_broj = int(input("Unesite prvi broj: "))
+        print(provjera_broja(prvi_broj))
+        drugi_broj = int(input("Unesite drugi broj: "))
+        print(provjera_broja(drugi_broj))
+
     except ValueError:
         print("Unesena vrijednost nije broj.")
